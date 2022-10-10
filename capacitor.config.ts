a74a2ac3e2138dda80c918cc7eb742ba9bb1a065
omariip <aboushaar.omar@gmail.com>
2022-10-10 03:02:04 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'technicianAdmin',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
